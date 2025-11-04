67bf9ac48708dedd6e8ce6553f73359a3456f3f6
Maruti Chavan <chavan.maruti2307@gmail.com>
2025-11-04 21:03:04 +0530

    Create alb.tf

---
+++ alb.tf
@@ -0,0 +1,31 @@
+resource "aws_lb" "app_alb" {
+  name               = "${var.project}-alb"
+  internal           = false
+  load_balancer_type = "application"
+  subnets            = aws_subnet.public[*].id
+  security_groups    = [aws_security_group.alb_sg.id]
+
+  tags = { Name = "${var.project}-alb" }
+}
+
+resource "aws_lb_target_group" "app_tg" {
+  name     = "${var.project}-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+  health_check {
+    path = "/"
+  }
+  tags = { Name = "${var.project}-tg" }
+}
+
+resource "aws_lb_listener" "http_listener" {
+  load_balancer_arn = aws_lb.app_alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app_tg.arn
+  }
+}
